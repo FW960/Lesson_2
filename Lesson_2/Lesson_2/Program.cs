fddc519319b892d9adc58b3f615bae393069b995
FW960 <artuninp@gmail.com>
2022-01-25 20:58:31 +0800

    Средняя Температура

--- Lesson_2/Lesson_2/Program.cs
+++ Lesson_2/Lesson_2/Program.cs
@@ -10,6 +10,19 @@ namespace Lesson_2
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Введите максимальную температуру за сегодняшний день:");
+            string max = Console.ReadLine();
+            float Max = Convert.ToSingle(max);
+            Console.WriteLine("Введите минимальную температуру за сегодняшний день:");
+            string min = Console.ReadLine();
+            float Min = Convert.ToSingle(min);
+            bool Weather = Max > Min;
+            if (Weather == true)
+            {
+                Console.WriteLine((Max + Min) / 2);
+            }
+            else return;
+            
         }
     }
 }
